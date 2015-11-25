d4b51e345df441cf477da5ff599152d55c5c9ff6
shahidulhasan <er.shahidul@gmail.com>
2015-11-25 22:09:18 +0600

    modify repository

---
+++ src/main/java/com/rbs/sfi/core/repositories/SfiPpFormOtherCountryRepository.java
@@ -0,0 +1,19 @@
+package com.rbs.sfi.core.repositories;
+
+import com.rbs.sfi.admin.repositories.AbstractRepository;
+import com.rbs.sfi.core.entities.SfiPpFormOtherCountry;
+import org.hibernate.SessionFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+@Repository("sfiPpFormOtherCountryRepository")
+public class SfiPpFormOtherCountryRepository extends AbstractRepository<Integer, SfiPpFormOtherCountry> {
+
+    @Autowired
+    SessionFactory sessionFactory;
+
+    public SfiPpFormOtherCountry findById(int id) {
+        return (SfiPpFormOtherCountry)sessionFactory.getCurrentSession().get(SfiPpFormOtherCountry.class, id);
+    }
+
+}
---
+++ src/main/java/com/rbs/sfi/core/repositories/SfiPpFormRegionRepository.java
@@ -0,0 +1,25 @@
+package com.rbs.sfi.core.repositories;
+
+import com.rbs.sfi.admin.repositories.AbstractRepository;
+import com.rbs.sfi.core.entities.SfiPpFormRegion;
+import org.hibernate.SessionFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Repository("sfiPpFormRegionRepository")
+public class SfiPpFormRegionRepository extends AbstractRepository<Integer, SfiPpFormRegion> {
+
+    @Autowired
+    SessionFactory sessionFactory;
+
+    @Transactional
+    public List<SfiPpFormRegion> list() {
+        @SuppressWarnings("unchecked")
+        List<SfiPpFormRegion> sfiPpFormRegion = sessionFactory.getCurrentSession().createQuery("from SfiPpFormRegion").list();
+        return sfiPpFormRegion;
+    }
+
+}
