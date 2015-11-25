5f3fc717764e923f33d395d55153d492ab5b4ef9
shahidulhasan <er.shahidul@gmail.com>
2015-11-25 22:04:26 +0600

    modify service

---
+++ src/main/java/com/rbs/sfi/core/services/SfiPpFormAllCountryService.java
@@ -0,0 +1,26 @@
+package com.rbs.sfi.core.services;
+
+import com.rbs.sfi.core.entities.SfiPpFormAllCountry;
+import com.rbs.sfi.core.repositories.SfiPpFormAllCountryRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service("sfiPpFormAllCountryService")
+@Transactional
+public class SfiPpFormAllCountryService
+{
+    @Autowired
+    SfiPpFormAllCountryRepository sfiPpFormAllCountryRepository;
+
+    public List<SfiPpFormAllCountry> list() {
+        return sfiPpFormAllCountryRepository.list();
+    }
+
+    public SfiPpFormAllCountry findById(int id) {
+        return sfiPpFormAllCountryRepository.findById(id);
+    }
+
+}
---
+++ src/main/java/com/rbs/sfi/core/services/SfiPpFormOtherCountryService.java
@@ -0,0 +1,20 @@
+package com.rbs.sfi.core.services;
+
+import com.rbs.sfi.core.entities.SfiPpFormOtherCountry;
+import com.rbs.sfi.core.repositories.SfiPpFormOtherCountryRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service("sfiPpFormOtherCountryService")
+@Transactional
+public class SfiPpFormOtherCountryService
+{
+    @Autowired
+    SfiPpFormOtherCountryRepository sfiPpFormOtherCountryRepository;
+
+    public SfiPpFormOtherCountry findById(int id) {
+        return sfiPpFormOtherCountryRepository.findById(id);
+    }
+
+}
