f21d9d376240fa3ecfc49853ad55de807e284f9a
shahidulhasan <er.shahidul@gmail.com>
2015-11-27 16:51:55 +0600

    add Cs2ViewModel entity

---
+++ src/main/java/com/rbs/sfi/core/models/viewmodels/Cs2ViewModel.java
@@ -0,0 +1,278 @@
+package com.rbs.sfi.core.models.viewmodels;
+
+import com.rbs.sfi.core.models.entities.SfiPpFormCs2OutsideUsaCa;
+
+import java.util.List;
+
+public class Cs2ViewModel {
+	private Integer id;
+	private Float usaPrivateTotalAreaOwnOrManage;
+	private Integer usaPrivateForestlandOwnMng;
+	private String usaPrivateForestlandOwnMngOther;
+	private Float usaPrivateTotalAreaCertified;
+	private Float usaPrivateAreaManagedForPublic;
+	private Float usaPrivateAreaCertifiedForPublic;
+	private List<String> usaPrivateRecreationCategories;
+	private Float usaPublicTotalAreaOwnOrManage;
+	private Integer usaPublicForestlandOwnMng;
+	private String usaPublicForestlandOwnMngOther;
+	private Float usaPublicTotalAreaCertified;
+	private Float usaPublicAreaManagedForPublic;
+	private Float usaPublicAreaCertifiedForPublic;
+	private List<String> usaPublicRecreationCategories;
+	private Float caCrownTotalAreaOwnOrManage;
+	private Integer caCrownForestlandOwnMng;
+	private String caCrownForestlandOwnMngOther;
+	private Float caCrownTotalAreaCertified;
+	private Float caCrownAreaManagedForPublic;
+	private Float caCrownAreaCertifiedForPublic;
+	private List<String> caCrownRecreationCategories;
+	private Float caPrivateTotalAreaOwnOrManage;
+	private Integer caPrivateForestlandOwnMng;
+	private String caPrivateForestlandOwnMngOther;
+	private Float caPrivateTotalAreaCertified;
+	private Float caPrivateAreaManagedForPublic;
+	private Float caPrivateAreaCertifiedForPublic;
+	private List<String> caPrivateRecreationCategories;
+	private List<SfiPpFormCs2OutsideUsaCa> outsideCountries;
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public Float getUsaPrivateTotalAreaOwnOrManage() {
+		return usaPrivateTotalAreaOwnOrManage;
+	}
+
+	public void setUsaPrivateTotalAreaOwnOrManage(Float usaPrivateTotalAreaOwnOrManage) {
+		this.usaPrivateTotalAreaOwnOrManage = usaPrivateTotalAreaOwnOrManage;
+	}
+
+	public Integer getUsaPrivateForestlandOwnMng() {
+		return usaPrivateForestlandOwnMng;
+	}
+
+	public void setUsaPrivateForestlandOwnMng(Integer usaPrivateForestlandOwnMng) {
+		this.usaPrivateForestlandOwnMng = usaPrivateForestlandOwnMng;
+	}
+
+	public String getUsaPrivateForestlandOwnMngOther() {
+		return usaPrivateForestlandOwnMngOther;
+	}
+
+	public void setUsaPrivateForestlandOwnMngOther(String usaPrivateForestlandOwnMngOther) {
+		this.usaPrivateForestlandOwnMngOther = usaPrivateForestlandOwnMngOther;
+	}
+
+	public Float getUsaPrivateTotalAreaCertified() {
+		return usaPrivateTotalAreaCertified;
+	}
+
+	public void setUsaPrivateTotalAreaCertified(Float usaPrivateTotalAreaCertified) {
+		this.usaPrivateTotalAreaCertified = usaPrivateTotalAreaCertified;
+	}
+
+	public Float getUsaPrivateAreaManagedForPublic() {
+		return usaPrivateAreaManagedForPublic;
+	}
+
+	public void setUsaPrivateAreaManagedForPublic(Float usaPrivateAreaManagedForPublic) {
+		this.usaPrivateAreaManagedForPublic = usaPrivateAreaManagedForPublic;
+	}
+
+	public Float getUsaPrivateAreaCertifiedForPublic() {
+		return usaPrivateAreaCertifiedForPublic;
+	}
+
+	public void setUsaPrivateAreaCertifiedForPublic(Float usaPrivateAreaCertifiedForPublic) {
+		this.usaPrivateAreaCertifiedForPublic = usaPrivateAreaCertifiedForPublic;
+	}
+
+	public List<String> getUsaPrivateRecreationCategories() {
+		return usaPrivateRecreationCategories;
+	}
+
+	public void setUsaPrivateRecreationCategories(List<String> usaPrivateRecreationCategories) {
+		this.usaPrivateRecreationCategories = usaPrivateRecreationCategories;
+	}
+
+	public Float getUsaPublicTotalAreaOwnOrManage() {
+		return usaPublicTotalAreaOwnOrManage;
+	}
+
+	public void setUsaPublicTotalAreaOwnOrManage(Float usaPublicTotalAreaOwnOrManage) {
+		this.usaPublicTotalAreaOwnOrManage = usaPublicTotalAreaOwnOrManage;
+	}
+
+	public Integer getUsaPublicForestlandOwnMng() {
+		return usaPublicForestlandOwnMng;
+	}
+
+	public void setUsaPublicForestlandOwnMng(Integer usaPublicForestlandOwnMng) {
+		this.usaPublicForestlandOwnMng = usaPublicForestlandOwnMng;
+	}
+
+	public String getUsaPublicForestlandOwnMngOther() {
+		return usaPublicForestlandOwnMngOther;
+	}
+
+	public void setUsaPublicForestlandOwnMngOther(String usaPublicForestlandOwnMngOther) {
+		this.usaPublicForestlandOwnMngOther = usaPublicForestlandOwnMngOther;
+	}
+
+	public Float getUsaPublicTotalAreaCertified() {
+		return usaPublicTotalAreaCertified;
+	}
+
+	public void setUsaPublicTotalAreaCertified(Float usaPublicTotalAreaCertified) {
+		this.usaPublicTotalAreaCertified = usaPublicTotalAreaCertified;
+	}
+
+	public Float getUsaPublicAreaManagedForPublic() {
+		return usaPublicAreaManagedForPublic;
+	}
+
+	public void setUsaPublicAreaManagedForPublic(Float usaPublicAreaManagedForPublic) {
+		this.usaPublicAreaManagedForPublic = usaPublicAreaManagedForPublic;
+	}
+
+	public Float getUsaPublicAreaCertifiedForPublic() {
+		return usaPublicAreaCertifiedForPublic;
+	}
+
+	public void setUsaPublicAreaCertifiedForPublic(Float usaPublicAreaCertifiedForPublic) {
+		this.usaPublicAreaCertifiedForPublic = usaPublicAreaCertifiedForPublic;
+	}
+
+	public List<String> getUsaPublicRecreationCategories() {
+		return usaPublicRecreationCategories;
+	}
+
+	public void setUsaPublicRecreationCategories(List<String> usaPublicRecreationCategories) {
+		this.usaPublicRecreationCategories = usaPublicRecreationCategories;
+	}
+
+	public Float getCaCrownTotalAreaOwnOrManage() {
+		return caCrownTotalAreaOwnOrManage;
+	}
+
+	public void setCaCrownTotalAreaOwnOrManage(Float caCrownTotalAreaOwnOrManage) {
+		this.caCrownTotalAreaOwnOrManage = caCrownTotalAreaOwnOrManage;
+	}
+
+	public Integer getCaCrownForestlandOwnMng() {
+		return caCrownForestlandOwnMng;
+	}
+
+	public void setCaCrownForestlandOwnMng(Integer caCrownForestlandOwnMng) {
+		this.caCrownForestlandOwnMng = caCrownForestlandOwnMng;
+	}
+
+	public String getCaCrownForestlandOwnMngOther() {
+		return caCrownForestlandOwnMngOther;
+	}
+
+	public void setCaCrownForestlandOwnMngOther(String caCrownForestlandOwnMngOther) {
+		this.caCrownForestlandOwnMngOther = caCrownForestlandOwnMngOther;
+	}
+
+	public Float getCaCrownTotalAreaCertified() {
+		return caCrownTotalAreaCertified;
+	}
+
+	public void setCaCrownTotalAreaCertified(Float caCrownTotalAreaCertified) {
+		this.caCrownTotalAreaCertified = caCrownTotalAreaCertified;
+	}
+
+	public Float getCaCrownAreaManagedForPublic() {
+		return caCrownAreaManagedForPublic;
+	}
+
+	public void setCaCrownAreaManagedForPublic(Float caCrownAreaManagedForPublic) {
+		this.caCrownAreaManagedForPublic = caCrownAreaManagedForPublic;
+	}
+
+	public Float getCaCrownAreaCertifiedForPublic() {
+		return caCrownAreaCertifiedForPublic;
+	}
+
+	public void setCaCrownAreaCertifiedForPublic(Float caCrownAreaCertifiedForPublic) {
+		this.caCrownAreaCertifiedForPublic = caCrownAreaCertifiedForPublic;
+	}
+
+	public List<String> getCaCrownRecreationCategories() {
+		return caCrownRecreationCategories;
+	}
+
+	public void setCaCrownRecreationCategories(List<String> caCrownRecreationCategories) {
+		this.caCrownRecreationCategories = caCrownRecreationCategories;
+	}
+
+	public Float getCaPrivateTotalAreaOwnOrManage() {
+		return caPrivateTotalAreaOwnOrManage;
+	}
+
+	public void setCaPrivateTotalAreaOwnOrManage(Float caPrivateTotalAreaOwnOrManage) {
+		this.caPrivateTotalAreaOwnOrManage = caPrivateTotalAreaOwnOrManage;
+	}
+
+	public Integer getCaPrivateForestlandOwnMng() {
+		return caPrivateForestlandOwnMng;
+	}
+
+	public void setCaPrivateForestlandOwnMng(Integer caPrivateForestlandOwnMng) {
+		this.caPrivateForestlandOwnMng = caPrivateForestlandOwnMng;
+	}
+
+	public String getCaPrivateForestlandOwnMngOther() {
+		return caPrivateForestlandOwnMngOther;
+	}
+
+	public void setCaPrivateForestlandOwnMngOther(String caPrivateForestlandOwnMngOther) {
+		this.caPrivateForestlandOwnMngOther = caPrivateForestlandOwnMngOther;
+	}
+
+	public Float getCaPrivateTotalAreaCertified() {
+		return caPrivateTotalAreaCertified;
+	}
+
+	public void setCaPrivateTotalAreaCertified(Float caPrivateTotalAreaCertified) {
+		this.caPrivateTotalAreaCertified = caPrivateTotalAreaCertified;
+	}
+
+	public Float getCaPrivateAreaManagedForPublic() {
+		return caPrivateAreaManagedForPublic;
+	}
+
+	public void setCaPrivateAreaManagedForPublic(Float caPrivateAreaManagedForPublic) {
+		this.caPrivateAreaManagedForPublic = caPrivateAreaManagedForPublic;
+	}
+
+	public Float getCaPrivateAreaCertifiedForPublic() {
+		return caPrivateAreaCertifiedForPublic;
+	}
+
+	public void setCaPrivateAreaCertifiedForPublic(Float caPrivateAreaCertifiedForPublic) {
+		this.caPrivateAreaCertifiedForPublic = caPrivateAreaCertifiedForPublic;
+	}
+
+	public List<String> getCaPrivateRecreationCategories() {
+		return caPrivateRecreationCategories;
+	}
+
+	public void setCaPrivateRecreationCategories(List<String> caPrivateRecreationCategories) {
+		this.caPrivateRecreationCategories = caPrivateRecreationCategories;
+	}
+
+	public List<SfiPpFormCs2OutsideUsaCa> getOutsideCountries() {
+		return outsideCountries;
+	}
+
+	public void setOutsideCountries(List<SfiPpFormCs2OutsideUsaCa> outsideCountries) {
+		this.outsideCountries = outsideCountries;
+	}
+}
