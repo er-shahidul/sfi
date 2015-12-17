0107508c64d773496d8eb152e6f7e5726c447a8d
shahidulhasan <er.shahidul@gmail.com>
2015-12-17 14:52:02 +0600

    cs10 field add

--- src/main/java/com/rbs/sfi/web/models/entities/Cs10.java
+++ src/main/java/com/rbs/sfi/web/models/entities/Cs10.java
@@ -1,9 +1,12 @@
 package com.rbs.sfi.web.models.entities;
 
 import com.rbs.sfi.common.models.BaseEntityModel;
+import org.hibernate.annotations.LazyCollection;
+import org.hibernate.annotations.LazyCollectionOption;
 
 import javax.persistence.*;
 import java.util.Date;
+import java.util.Set;
 
 @Entity
 @Table(name = "sfi_pp_forms")
@@ -15,14 +18,65 @@ public class Cs10 extends BaseEntityModel {
     @Column(name = "cs10_acknowledge", nullable = true)
     private Boolean acknowledge;
 
-    @Column(name = "updated_at", nullable = true)
-    private Date updatedAt;
+    @Column(name = "cs10_legalComplianceOutside", nullable = true)
+    private Boolean legalComplianceOutside;
+
+    @Column(name = "cs10_legalComplianceUsCanada", nullable = true)
+    private Boolean legalComplianceUsCanada;
 
     @Column(name = "created_at", nullable = true)
     private Date createdAt;
 
     @Column(name = "submitted_at", nullable = true)
     private Date submittedAt;
+
+    @Column(name = "updated_at", nullable = true)
+    private Date updatedAt;
+
+    @Column(name = "cs10_biodiversityItems", nullable = true)
+    @ElementCollection
+    @LazyCollection(LazyCollectionOption.FALSE)
+    private Set<Integer>biodiversityItems;
+
+    @Column(name = "cs10_biodiversityManagementItems", nullable = true)
+    @ElementCollection
+    @LazyCollection(LazyCollectionOption.FALSE)
+    private Set<Integer>biodiversityManagementItems;
+
+    @Column(name = "cs10_biodiversityNatureItems", nullable = true)
+    @ElementCollection
+    @LazyCollection(LazyCollectionOption.FALSE)
+    private Set<Integer>biodiversityNatureItems;
+
+    @Column(name = "cs10_biodiversityStateForestItems", nullable = true)
+    @ElementCollection
+    @LazyCollection(LazyCollectionOption.FALSE)
+    private Set<Integer>biodiversityStateForestItems;
+
+    @Column(name = "cs10_biodiversityTrainingItems", nullable = true)
+    @ElementCollection
+    @LazyCollection(LazyCollectionOption.FALSE)
+    private Set<Integer>biodiversityTrainingItems;
+
+    @Column(name = "cs10_legalComplianceOutsideItems", nullable = true)
+    @ElementCollection
+    @LazyCollection(LazyCollectionOption.FALSE)
+    private Set<Integer> legalComplianceOutsideItems;
+
+    @Column(name = "cs10_biodiversityFiberSourcing", nullable = true)
+    private String biodiversityFiberSourcing;
+
+    @Column(name = "cs10_biodiversityManagement", nullable = true)
+    private String biodiversityManagement;
+
+    @Column(name = "cs10_biodiversityTraining", nullable = true)
+    private String biodiversityTraining;
+
+    @Column(name = "cs10_legalComplianceOutsideOtherExplain", nullable = true)
+    private String legalComplianceOutsideOtherExplain;
+
+    @Column(name = "cs10_trainedWoodProducersChanged", nullable = true)
+    private String trainedWoodProducersChanged;
 
     @Override
     public Integer getId() {
@@ -41,12 +95,20 @@ public class Cs10 extends BaseEntityModel {
         this.acknowledge = acknowledge;
     }
 
-    public Date getUpdatedAt() {
-        return updatedAt;
+    public Boolean getLegalComplianceOutside() {
+        return legalComplianceOutside;
     }
 
-    public void setUpdatedAt(Date updatedAt) {
-        this.updatedAt = updatedAt;
+    public void setLegalComplianceOutside(Boolean legalComplianceOutside) {
+        this.legalComplianceOutside = legalComplianceOutside;
+    }
+
+    public Boolean getLegalComplianceUsCanada() {
+        return legalComplianceUsCanada;
+    }
+
+    public void setLegalComplianceUsCanada(Boolean legalComplianceUsCanada) {
+        this.legalComplianceUsCanada = legalComplianceUsCanada;
     }
 
     public Date getCreatedAt() {
@@ -63,5 +125,101 @@ public class Cs10 extends BaseEntityModel {
 
     public void setSubmittedAt(Date submittedAt) {
         this.submittedAt = submittedAt;
+    }
+
+    public Date getUpdatedAt() {
+        return updatedAt;
+    }
+
+    public void setUpdatedAt(Date updatedAt) {
+        this.updatedAt = updatedAt;
+    }
+
+    public Set<Integer> getBiodiversityItems() {
+        return biodiversityItems;
+    }
+
+    public void setBiodiversityItems(Set<Integer> biodiversityItems) {
+        addAll(this.biodiversityItems, biodiversityItems);
+    }
+
+    public Set<Integer> getBiodiversityManagementItems() {
+        return biodiversityManagementItems;
+    }
+
+    public void setBiodiversityManagementItems(Set<Integer> biodiversityManagementItems) {
+        addAll(this.biodiversityManagementItems, biodiversityManagementItems);
+    }
+
+    public Set<Integer> getBiodiversityNatureItems() {
+        return biodiversityNatureItems;
+    }
+
+    public void setBiodiversityNatureItems(Set<Integer> biodiversityNatureItems) {
+        addAll(this.biodiversityNatureItems, biodiversityNatureItems);
+    }
+
+    public Set<Integer> getBiodiversityStateForestItems() {
+        return biodiversityStateForestItems;
+    }
+
+    public void setBiodiversityStateForestItems(Set<Integer> biodiversityStateForestItems) {
+        addAll(this.biodiversityStateForestItems, biodiversityStateForestItems);
+    }
+
+    public Set<Integer> getBiodiversityTrainingItems() {
+        return biodiversityTrainingItems;
+    }
+
+    public void setBiodiversityTrainingItems(Set<Integer> biodiversityTrainingItems) {
+        addAll(this.biodiversityTrainingItems, biodiversityTrainingItems);
+    }
+
+    public Set<Integer> getLegalComplianceOutsideItems() {
+        return legalComplianceOutsideItems;
+    }
+
+    public void setLegalComplianceOutsideItems(Set<Integer> legalComplianceOutsideItems) {
+        addAll(this.legalComplianceOutsideItems, legalComplianceOutsideItems);
+    }
+
+    public String getBiodiversityFiberSourcing() {
+        return biodiversityFiberSourcing;
+    }
+
+    public void setBiodiversityFiberSourcing(String biodiversityFiberSourcing) {
+        this.biodiversityFiberSourcing = biodiversityFiberSourcing;
+    }
+
+    public String getBiodiversityManagement() {
+        return biodiversityManagement;
+    }
+
+    public void setBiodiversityManagement(String biodiversityManagement) {
+        this.biodiversityManagement = biodiversityManagement;
+    }
+
+    public String getBiodiversityTraining() {
+        return biodiversityTraining;
+    }
+
+    public void setBiodiversityTraining(String biodiversityTraining) {
+        this.biodiversityTraining = biodiversityTraining;
+    }
+
+    public String getLegalComplianceOutsideOtherExplain() {
+        return legalComplianceOutsideOtherExplain;
+    }
+
+    public void setLegalComplianceOutsideOtherExplain(String legalComplianceOutsideOtherExplain) {
+        this.legalComplianceOutsideOtherExplain = legalComplianceOutsideOtherExplain;
+    }
+
+    public String getTrainedWoodProducersChanged() {
+        return trainedWoodProducersChanged;
+    }
+
+    public void setTrainedWoodProducersChanged(String trainedWoodProducersChanged) {
+        this.trainedWoodProducersChanged = trainedWoodProducersChanged;
     }
 }
--- src/main/java/com/rbs/sfi/web/models/viewmodels/Cs10ViewModel.java
+++ src/main/java/com/rbs/sfi/web/models/viewmodels/Cs10ViewModel.java
@@ -3,13 +3,27 @@ package com.rbs.sfi.web.models.viewmodels;
 import com.rbs.sfi.common.models.BaseViewModel;
 
 import java.util.Date;
+import java.util.Set;
 
 public class Cs10ViewModel extends BaseViewModel {
     private Integer id;
     private Boolean acknowledge;
-    private Date updatedAt;
+    private Boolean legalComplianceOutside;
+    private Boolean legalComplianceUsCanada;
     private Date createdAt;
     private Date submittedAt;
+    private Date updatedAt;
+    private Set<Integer>biodiversityItems;
+    private Set<Integer>biodiversityManagementItems;
+    private Set<Integer>biodiversityNatureItems;
+    private Set<Integer>biodiversityStateForestItems;
+    private Set<Integer>biodiversityTrainingItems;
+    private Set<Integer> legalComplianceOutsideItems;
+    private String biodiversityFiberSourcing;
+    private String biodiversityManagement;
+    private String biodiversityTraining;
+    private String legalComplianceOutsideOtherExplain;
+    private String trainedWoodProducersChanged;
 
     @Override
     public Integer getId() {
@@ -28,12 +42,20 @@ public class Cs10ViewModel extends BaseViewModel {
         this.acknowledge = acknowledge;
     }
 
-    public Date getUpdatedAt() {
-        return updatedAt;
+    public Boolean getLegalComplianceOutside() {
+        return legalComplianceOutside;
     }
 
-    public void setUpdatedAt(Date updatedAt) {
-        this.updatedAt = updatedAt;
+    public void setLegalComplianceOutside(Boolean legalComplianceOutside) {
+        this.legalComplianceOutside = legalComplianceOutside;
+    }
+
+    public Boolean getLegalComplianceUsCanada() {
+        return legalComplianceUsCanada;
+    }
+
+    public void setLegalComplianceUsCanada(Boolean legalComplianceUsCanada) {
+        this.legalComplianceUsCanada = legalComplianceUsCanada;
     }
 
     public Date getCreatedAt() {
@@ -50,5 +72,101 @@ public class Cs10ViewModel extends BaseViewModel {
 
     public void setSubmittedAt(Date submittedAt) {
         this.submittedAt = submittedAt;
+    }
+
+    public Date getUpdatedAt() {
+        return updatedAt;
+    }
+
+    public void setUpdatedAt(Date updatedAt) {
+        this.updatedAt = updatedAt;
+    }
+
+    public Set<Integer> getBiodiversityItems() {
+        return biodiversityItems;
+    }
+
+    public void setBiodiversityItems(Set<Integer> biodiversityItems) {
+        this.biodiversityItems = biodiversityItems;
+    }
+
+    public Set<Integer> getBiodiversityManagementItems() {
+        return biodiversityManagementItems;
+    }
+
+    public void setBiodiversityManagementItems(Set<Integer> biodiversityManagementItems) {
+        this.biodiversityManagementItems = biodiversityManagementItems;
+    }
+
+    public Set<Integer> getBiodiversityNatureItems() {
+        return biodiversityNatureItems;
+    }
+
+    public void setBiodiversityNatureItems(Set<Integer> biodiversityNatureItems) {
+        this.biodiversityNatureItems = biodiversityNatureItems;
+    }
+
+    public Set<Integer> getBiodiversityStateForestItems() {
+        return biodiversityStateForestItems;
+    }
+
+    public void setBiodiversityStateForestItems(Set<Integer> biodiversityStateForestItems) {
+        this.biodiversityStateForestItems = biodiversityStateForestItems;
+    }
+
+    public Set<Integer> getBiodiversityTrainingItems() {
+        return biodiversityTrainingItems;
+    }
+
+    public void setBiodiversityTrainingItems(Set<Integer> biodiversityTrainingItems) {
+        this.biodiversityTrainingItems = biodiversityTrainingItems;
+    }
+
+    public Set<Integer> getLegalComplianceOutsideItems() {
+        return legalComplianceOutsideItems;
+    }
+
+    public void setLegalComplianceOutsideItems(Set<Integer> legalComplianceOutsideItems) {
+        this.legalComplianceOutsideItems = legalComplianceOutsideItems;
+    }
+
+    public String getBiodiversityFiberSourcing() {
+        return biodiversityFiberSourcing;
+    }
+
+    public void setBiodiversityFiberSourcing(String biodiversityFiberSourcing) {
+        this.biodiversityFiberSourcing = biodiversityFiberSourcing;
+    }
+
+    public String getBiodiversityManagement() {
+        return biodiversityManagement;
+    }
+
+    public void setBiodiversityManagement(String biodiversityManagement) {
+        this.biodiversityManagement = biodiversityManagement;
+    }
+
+    public String getBiodiversityTraining() {
+        return biodiversityTraining;
+    }
+
+    public void setBiodiversityTraining(String biodiversityTraining) {
+        this.biodiversityTraining = biodiversityTraining;
+    }
+
+    public String getLegalComplianceOutsideOtherExplain() {
+        return legalComplianceOutsideOtherExplain;
+    }
+
+    public void setLegalComplianceOutsideOtherExplain(String legalComplianceOutsideOtherExplain) {
+        this.legalComplianceOutsideOtherExplain = legalComplianceOutsideOtherExplain;
+    }
+
+    public String getTrainedWoodProducersChanged() {
+        return trainedWoodProducersChanged;
+    }
+
+    public void setTrainedWoodProducersChanged(String trainedWoodProducersChanged) {
+        this.trainedWoodProducersChanged = trainedWoodProducersChanged;
     }
 }
