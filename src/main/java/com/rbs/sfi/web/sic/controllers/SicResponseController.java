ce5881d600e826ebf830df0367d13b0e69181ed6
shahidulhasan <er.shahidul@gmail.com>
2015-12-21 14:31:33 +0600

    sic section controller modify

--- src/main/java/com/rbs/sfi/web/sic/controllers/SicResponseController.java
+++ src/main/java/com/rbs/sfi/web/sic/controllers/SicResponseController.java
@@ -2,6 +2,7 @@ package com.rbs.sfi.web.sic.controllers;
 
 import com.rbs.sfi.admin.util.Util;
 import com.rbs.sfi.web.sic.models.viewmodels.*;
+import com.rbs.sfi.web.sic.services.SicFormService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -13,30 +14,30 @@ import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
 @RestController
 public class SicResponseController {
 
-//    @Autowired
-//    private SicFormService sicFormService;
-//
-//    @RequestMapping(value = "/form/sic/cs1", method = RequestMethod.PUT, consumes = {APPLICATION_JSON_VALUE})
-//    public ResponseEntity<String> formCs1(@RequestBody Cs1ViewModel model, BindingResult result) {
-//        sicFormService.setCs1Entity(model);
-//        return new ResponseEntity<String>(Util.getAsString(model), HttpStatus.OK);
-//    }
-//
-//    @RequestMapping(value = "/form/sic/cs2", method = RequestMethod.PUT, consumes = {APPLICATION_JSON_VALUE})
-//    public ResponseEntity<String> formCs2(@RequestBody Cs2ViewModel model, BindingResult result) {
-//        sicFormService.setCs2Entity(model);
-//        return new ResponseEntity<String>(Util.getAsString(model), HttpStatus.OK);
-//    }
-//
-//    @RequestMapping(value = "/form/sic/cs3", method = RequestMethod.PUT, consumes = {APPLICATION_JSON_VALUE})
-//    public ResponseEntity<String> formCs3(@RequestBody Cs3ViewModel model, BindingResult result) {
-//        sicFormService.setCs3Entity(model);
-//        return new ResponseEntity<String>(Util.getAsString(model), HttpStatus.OK);
-//    }
-//
-//    @RequestMapping(value = "/form/sic/cs4", method = RequestMethod.PUT, consumes = {APPLICATION_JSON_VALUE})
-//    public ResponseEntity<String> formCs4(@RequestBody Cs4ViewModel model, BindingResult result) {
-//        sicFormService.setCs4Entity(model);
-//        return new ResponseEntity<String>(Util.getAsString(model), HttpStatus.OK);
-//    }
+    @Autowired
+    private SicFormService sicFormService;
+
+    @RequestMapping(value = "/form/sic/cs1", method = RequestMethod.PUT, consumes = {APPLICATION_JSON_VALUE})
+    public ResponseEntity<String> formCs1(@RequestBody SicCs1ViewModel model, BindingResult result) {
+        sicFormService.setCs1Entity(model);
+        return new ResponseEntity<String>(Util.getAsString(model), HttpStatus.OK);
+    }
+
+    @RequestMapping(value = "/form/sic/cs2", method = RequestMethod.PUT, consumes = {APPLICATION_JSON_VALUE})
+    public ResponseEntity<String> formCs2(@RequestBody SicCs2ViewModel model, BindingResult result) {
+        sicFormService.setCs2Entity(model);
+        return new ResponseEntity<String>(Util.getAsString(model), HttpStatus.OK);
+    }
+
+    @RequestMapping(value = "/form/sic/cs3", method = RequestMethod.PUT, consumes = {APPLICATION_JSON_VALUE})
+    public ResponseEntity<String> formCs3(@RequestBody SicCs3ViewModel model, BindingResult result) {
+        sicFormService.setCs3Entity(model);
+        return new ResponseEntity<String>(Util.getAsString(model), HttpStatus.OK);
+    }
+
+    @RequestMapping(value = "/form/sic/cs4", method = RequestMethod.PUT, consumes = {APPLICATION_JSON_VALUE})
+    public ResponseEntity<String> formCs4(@RequestBody SicCs4ViewModel model, BindingResult result) {
+        sicFormService.setCs4Entity(model);
+        return new ResponseEntity<String>(Util.getAsString(model), HttpStatus.OK);
+    }
 }
