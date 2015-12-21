a5c5860630ff2e1875f3c5751b595164332d3449
shahidulhasan <er.shahidul@gmail.com>
2015-12-21 13:01:00 +0600

    sic form entity and view model

---
+++ src/main/java/com/rbs/sfi/web/sic/models/entities/SicFormData.java
@@ -0,0 +1,22 @@
+package com.rbs.sfi.web.sic.models.entities;
+
+import com.rbs.sfi.common.models.BaseEntityModel;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "sic_pp_forms")
+public class SicFormData extends BaseEntityModel {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Override
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+}
---
+++ src/main/java/com/rbs/sfi/web/sic/models/viewmodels/Cs1ViewModel.java
@@ -0,0 +1,16 @@
+package com.rbs.sfi.web.sic.models.viewmodels;
+
+import com.rbs.sfi.common.models.BaseViewModel;
+
+public class Cs1ViewModel extends BaseViewModel {
+    private Integer id;
+
+    @Override
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+}
