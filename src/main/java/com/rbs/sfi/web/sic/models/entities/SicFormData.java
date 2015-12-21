22c86328fb2e14c4d0dad537fa82cfe5978e30df
shahidulhasan <er.shahidul@gmail.com>
2015-12-21 14:30:59 +0600

    sicForm add some entity

--- src/main/java/com/rbs/sfi/web/sic/models/entities/SicFormData.java
+++ src/main/java/com/rbs/sfi/web/sic/models/entities/SicFormData.java
@@ -1,8 +1,12 @@
 package com.rbs.sfi.web.sic.models.entities;
 
+import com.rbs.sfi.admin.entities.Company;
+import com.rbs.sfi.admin.entities.User;
 import com.rbs.sfi.common.models.BaseEntityModel;
+import com.rbs.sfi.web.sfi.models.entities.SfiPpFormStatus;
 
 import javax.persistence.*;
+import java.util.Date;
 
 @Entity
 @Table(name = "sic_forms")
@@ -11,6 +15,30 @@ public class SicFormData extends BaseEntityModel {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
 
+    @ManyToOne(targetEntity = SfiPpFormStatus.class, optional = true)
+    @JoinColumn(name = "status_id", referencedColumnName = "id", nullable = true)
+    private SfiPpFormStatus status;
+
+    @OneToOne(targetEntity = Company.class, optional = false)
+    @JoinColumn(name = "company_id", referencedColumnName = "id", nullable = true)
+    private Company company;
+
+    @ManyToOne(targetEntity = User.class, optional = true)
+    @JoinColumn(name = "created_by", referencedColumnName = "id", nullable = true)
+    private User createdBy;
+
+    @ManyToOne(targetEntity = User.class, optional = true)
+    @JoinColumn(name = "updated_by", referencedColumnName = "id", nullable = true)
+    private User updatedBy;
+
+    @Temporal(TemporalType.TIMESTAMP)
+    @Column(name = "created_at")
+    private Date createdAt;
+
+    @Temporal(TemporalType.TIMESTAMP)
+    @Column(name = "updated_at")
+    private Date updatedAt;
+
     @Override
     public Integer getId() {
         return id;
@@ -18,5 +46,53 @@ public class SicFormData extends BaseEntityModel {
 
     public void setId(Integer id) {
         this.id = id;
+    }
+
+    public SfiPpFormStatus getStatus() {
+        return status;
+    }
+
+    public void setStatus(SfiPpFormStatus status) {
+        this.status = status;
+    }
+
+    public Company getCompany() {
+        return company;
+    }
+
+    public void setCompany(Company company) {
+        this.company = company;
+    }
+
+    public User getCreatedBy() {
+        return createdBy;
+    }
+
+    public void setCreatedBy(User createdBy) {
+        this.createdBy = createdBy;
+    }
+
+    public User getUpdatedBy() {
+        return updatedBy;
+    }
+
+    public void setUpdatedBy(User updatedBy) {
+        this.updatedBy = updatedBy;
+    }
+
+    public Date getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(Date createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public Date getUpdatedAt() {
+        return updatedAt;
+    }
+
+    public void setUpdatedAt(Date updatedAt) {
+        this.updatedAt = updatedAt;
     }
 }
