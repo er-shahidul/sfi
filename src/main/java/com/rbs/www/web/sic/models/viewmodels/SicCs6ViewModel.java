a64e1add1f59de9a969fc0117d139c6682f203e6
shahidulhasan <er.shahidul@gmail.com>
2016-01-07 23:34:53 +0600

    add field in sic step-6  entity and view model

--- src/main/java/com/rbs/www/web/sic/models/viewmodels/SicCs6ViewModel.java
+++ src/main/java/com/rbs/www/web/sic/models/viewmodels/SicCs6ViewModel.java
@@ -2,8 +2,13 @@ package com.rbs.www.web.sic.models.viewmodels;
 
 import com.rbs.www.common.models.BaseViewModel;
 
+import java.util.Set;
+
 public class SicCs6ViewModel extends BaseViewModel {
     private Integer id;
+
+    private Set<Integer> trainingPrograms;
+    private Boolean certifiedLoggingProfessional;
 
     @Override
     public Integer getId() {
@@ -12,5 +17,21 @@ public class SicCs6ViewModel extends BaseViewModel {
 
     public void setId(Integer id) {
         this.id = id;
+    }
+
+    public Set<Integer> getTrainingPrograms() {
+        return trainingPrograms;
+    }
+
+    public void setTrainingPrograms(Set<Integer> trainingPrograms) {
+        this.trainingPrograms = trainingPrograms;
+    }
+
+    public Boolean getCertifiedLoggingProfessional() {
+        return certifiedLoggingProfessional;
+    }
+
+    public void setCertifiedLoggingProfessional(Boolean certifiedLoggingProfessional) {
+        this.certifiedLoggingProfessional = certifiedLoggingProfessional;
     }
 }
