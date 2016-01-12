6510b4ff34bfd0e4fa637ca3424cc58db5a1f732
Asad Rahman <asad.dk.bd@gmail.com>
2016-01-13 00:58:16 +0600

    Sic Step 8

--- src/main/webapp/assets/js/sicApp/controllers/SicCs8FormCtrl.js
+++ src/main/webapp/assets/js/sicApp/controllers/SicCs8FormCtrl.js
@@ -1,15 +1,57 @@
-
+var cs8;
 sfiSicApp
-    .controller('SicCs8FormCtrl', ['$scope', '$rootScope', '$http', 'Message',
-        function ( $scope, $rootScope, $http, Message){
+    .controller('SicCs8FormCtrl', ['$scope', '$rootScope', '$http', 'Message', 'standardObjects',
+        function ( $scope, $rootScope, $http, Message, standardObjects){
+
+        cs8 = $scope;
 
         $scope.cs8 = angular.copy($rootScope.form.cs8);
+        $scope.standardObjects = standardObjects;
 
-        $scope.bmpReportDoc = {};
-        $scope.cs8.bmpReportDoc = $scope.cs8.bmpReportDoc || {};
+        $scope.project  = {}
+        $scope.cs8.projects = $scope.cs8.projects || [];
+
+
+        $scope.isProjectDirty = function(){
+
+            return $scope.project.name ||
+                $scope.project.organization ||
+                $scope.project.contactName ||
+                $scope.project.contactEmail ||
+                $scope.project.description ;
+        }
+
+
+        $scope.projectIndex = null;
+
+        $scope.addProject = function(){
+
+            if($scope.projectForm.$valid){
+
+                if($scope.projectIndex){
+                    $scope.cs8.projects[$scope.projectIndex]  = angular.copy($scope.project);
+                }else{
+                    $scope.cs8.projects.push(angular.copy($scope.project));
+                }
+            }
+
+            $scope.project = {};
+        }
+
+        $scope.editProject = function($index){
+            $scope.project = angular.copy($scope.cs8.projects[$index]);
+        }
+
+        $scope.deleteProject = function($index){
+
+            $scope.cs8.projects.splice($index, 1);
+        }
+
 
 
         $scope.saveForm = function(go){
+
+            $scope.addProject();
 
             if($scope.cs8Form.$invalid){
                 return false;
