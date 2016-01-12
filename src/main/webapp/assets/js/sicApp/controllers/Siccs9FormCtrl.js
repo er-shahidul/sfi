eb306bd513407e5e336f438f70ce848d4105ad7b
Asad Rahman <asad.dk.bd@gmail.com>
2016-01-13 02:47:14 +0600

    Sic Step 9

--- src/main/webapp/assets/js/sicApp/controllers/Siccs9FormCtrl.js
+++ src/main/webapp/assets/js/sicApp/controllers/Siccs9FormCtrl.js
@@ -5,7 +5,27 @@ sfiSicApp
         $scope.cs9 = angular.copy($rootScope.form.cs9);
 
         $scope.bmpReportDoc = {};
-        $scope.cs9.bmpReportDoc = $scope.cs9.bmpReportDoc || {};
+        $scope.cs9.supportDocPrinted = $scope.cs9.supportDocPrinted || {};
+
+        $scope.cs9.supportDocLastYrPrinted = $scope.cs9.supportDocLastYrPrinted || {};
+        $scope.cs9.supportDocLastYrAdvTV = $scope.cs9.supportDocLastYrAdvTV || {};
+        $scope.cs9.supportDocLastYrAdvPrinted = $scope.cs9.supportDocLastYrAdvPrinted || {};
+        $scope.cs9.supportDocLastYrTradeshow = $scope.cs9.supportDocLastYrTradeshow || {};
+        $scope.cs9.supportDocLastYrEventSponsor = $scope.cs9.supportDocLastYrEventSponsor || {};
+        $scope.cs9.supportDocLastYrSocialMedia = $scope.cs9.supportDocLastYrSocialMedia || {};
+        $scope.cs9.supportDocLastYrVideo = $scope.cs9.supportDocLastYrVideo || {};
+        $scope.cs9.supportDocLastYrOther = $scope.cs9.supportDocLastYrOther || {};
+
+        $scope.cs9.supportDocCurrentYrPrinted = $scope.cs9.supportDocCurrentYrPrinted || [];
+        $scope.cs9.supportDocCurrentYrAdvTV = $scope.cs9.supportDocCurrentYrAdvTV || [];
+        $scope.cs9.supportDocCurrentYrAdvPrinted = $scope.cs9.supportDocCurrentYrAdvPrinted || [];
+        $scope.cs9.supportDocCurrentYrTradeshow = $scope.cs9.supportDocCurrentYrTradeshow || [];
+        $scope.cs9.supportDocCurrentYrEventSponsor = $scope.cs9.supportDocCurrentYrEventSponsor || [];
+        $scope.cs9.supportDocCurrentYrSocialMedia = $scope.cs9.supportDocCurrentYrSocialMedia || [];
+        $scope.cs9.supportDocCurrentYrVideo = $scope.cs9.supportDocCurrentYrVideo || [];
+        $scope.cs9.supportDocCurrentYrOther = $scope.cs9.supportDocCurrentYrOther || [];
+
+
 
 
         $scope.saveForm = function(go){
@@ -20,7 +40,7 @@ sfiSicApp
 
                     if(response.data){
 
-                        $scope.cs6Form.$setPristine();
+                        $scope.cs9Form.$setPristine();
                         Message.success('Section successfully saved', '.msg-cont', true);
                     }
 
