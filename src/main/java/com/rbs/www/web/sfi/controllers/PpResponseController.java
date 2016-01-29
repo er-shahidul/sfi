8e258bc5983c13cc3ad60787e44c94696e3a12a5
shahidulhasan <er.shahidul@gmail.com>
2016-01-29 15:01:53 +0600

    pp response controller add validation function

--- src/main/java/com/rbs/www/web/sfi/controllers/PpResponseController.java
+++ src/main/java/com/rbs/www/web/sfi/controllers/PpResponseController.java
@@ -1,6 +1,8 @@
 package com.rbs.www.web.sfi.controllers;
 
+import com.rbs.www.common.models.BaseViewModel;
 import com.rbs.www.common.util.Util;
+import com.rbs.www.web.common.services.ModelValidationService;
 import com.rbs.www.web.sfi.models.entities.SfiPpFormData;
 import com.rbs.www.web.sfi.models.viewmodels.*;
 import com.rbs.www.web.sfi.services.FormService;
@@ -27,6 +29,8 @@ public class PpResponseController {
     @Autowired
     SfiPpFormDataService sfiPpFormDataService;
 
+    @Autowired
+    private ModelValidationService modelValidationService;
 
     @RequestMapping(value = "/pp/form/submit/{id}", method = RequestMethod.GET)
     public ResponseEntity<String> formSubmit(@PathVariable Integer id, ModelMap model) throws ParseException {
@@ -52,44 +56,44 @@ public class PpResponseController {
     @RequestMapping(value = "/form/cs1", method = RequestMethod.PUT, consumes = {APPLICATION_JSON_VALUE})
     public ResponseEntity<String> formCs1(@RequestBody Cs1ViewModel model, BindingResult result) {
         formService.setCs1Entity(model);
-        return new ResponseEntity<String>(Util.getAsString(model), HttpStatus.OK);
+        return new ResponseEntity<String>(Util.getAsString(this.validate(model)), HttpStatus.OK);
     }
 
     @RequestMapping(value = "/form/cs2", method = RequestMethod.PUT, consumes = {APPLICATION_JSON_VALUE})
     public ResponseEntity<String> formCs2(@RequestBody Cs2ViewModel model, BindingResult result) {
         formService.setCs2Entity(model);
-        return new ResponseEntity<String>(Util.getAsString(model), HttpStatus.OK);
+        return new ResponseEntity<String>(Util.getAsString(this.validate(model)), HttpStatus.OK);
     }
 
     @RequestMapping(value = "/form/cs3", method = RequestMethod.PUT, consumes = {APPLICATION_JSON_VALUE})
     public ResponseEntity<String> formCs3(@RequestBody Cs3ViewModel model, BindingResult result) {
         formService.setCs3Entity(model);
-        return new ResponseEntity<String>(Util.getAsString(model), HttpStatus.OK);
+        return new ResponseEntity<String>(Util.getAsString(this.validate(model)), HttpStatus.OK);
     }
 
     @RequestMapping(value = "/form/cs4", method = RequestMethod.PUT, consumes = {APPLICATION_JSON_VALUE})
     public ResponseEntity<String> formCs4(@RequestBody Cs4ViewModel model, BindingResult result) {
         formService.setCs4Entity(model);
-        return new ResponseEntity<String>(Util.getAsString(model), HttpStatus.OK);
+        return new ResponseEntity<String>(Util.getAsString(this.validate(model)), HttpStatus.OK);
     }
 
     @RequestMapping(value = "/form/cs5", method = RequestMethod.PUT, consumes = {APPLICATION_JSON_VALUE})
     public ResponseEntity<String> formCs5(@RequestBody Cs5ViewModel model, BindingResult result) {
         formService.setSfiPpFormCs5Entities(model.getItems());
         formService.setCs5Entity(model);
-        return new ResponseEntity<String>(Util.getAsString(model), HttpStatus.OK);
+        return new ResponseEntity<String>(Util.getAsString(this.validate(model)), HttpStatus.OK);
     }
 
     @RequestMapping(value = "/form/cs6", method = RequestMethod.PUT, consumes = {APPLICATION_JSON_VALUE})
     public ResponseEntity<String> formCs6(@RequestBody Cs6ViewModel model, BindingResult result) {
         formService.setCs6Entity(model);
-        return new ResponseEntity<String>(Util.getAsString(model), HttpStatus.OK);
+        return new ResponseEntity<String>(Util.getAsString(this.validate(model)), HttpStatus.OK);
     }
 
     @RequestMapping(value = "/form/cs7", method = RequestMethod.PUT, consumes = {APPLICATION_JSON_VALUE})
     public ResponseEntity<String> formCs7(@RequestBody Cs7ViewModel model, BindingResult result) {
         formService.setCs7Entity(model);
-        return new ResponseEntity<String>(Util.getAsString(model), HttpStatus.OK);
+        return new ResponseEntity<String>(Util.getAsString(this.validate(model)), HttpStatus.OK);
     }
 
     @RequestMapping(value = "/form/cs8", method = RequestMethod.PUT, consumes = {APPLICATION_JSON_VALUE})
@@ -101,12 +105,17 @@ public class PpResponseController {
     @RequestMapping(value = "/form/cs9", method = RequestMethod.PUT, consumes = {APPLICATION_JSON_VALUE})
     public ResponseEntity<String> formCs9(@RequestBody Cs9ViewModel model, BindingResult result) {
         formService.setCs9Entity(model);
-        return new ResponseEntity<String>(Util.getAsString(model), HttpStatus.OK);
+        return new ResponseEntity<String>(Util.getAsString(this.validate(model)), HttpStatus.OK);
     }
 
     @RequestMapping(value = "/form/cs10", method = RequestMethod.PUT, consumes = {APPLICATION_JSON_VALUE})
     public ResponseEntity<String> formCs10(@RequestBody Cs10ViewModel model, BindingResult result) {
         formService.setCs10Entity(model);
-        return new ResponseEntity<String>(Util.getAsString(model), HttpStatus.OK);
+        return new ResponseEntity<String>(Util.getAsString(this.validate(model)), HttpStatus.OK);
+    }
+
+    private <T extends BaseViewModel> T validate(T model) {
+        modelValidationService.validate(model);
+        return model;
     }
 }
