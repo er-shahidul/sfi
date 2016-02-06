2010a3fd9824b52fb94411f48cdf472fbf247e87
shahidulhasan <er.shahidul@gmail.com>
2016-02-06 17:26:12 +0600

    t-601

--- src/main/java/com/rbs/www/web/common/repositories/SfiPpFormRegionRepository.java
+++ src/main/java/com/rbs/www/web/common/repositories/SfiPpFormRegionRepository.java
@@ -2,8 +2,24 @@ package com.rbs.www.web.common.repositories;
 
 import com.rbs.www.common.repositories.BaseRepository;
 import com.rbs.www.web.common.models.entities.SfiPpFormRegion;
+import org.hibernate.criterion.Criterion;
+import org.hibernate.criterion.Order;
+import org.hibernate.criterion.Projections;
+import org.hibernate.criterion.Restrictions;
+import org.hibernate.transform.Transformers;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public class SfiPpFormRegionRepository extends BaseRepository<SfiPpFormRegion> {
+
+    @SuppressWarnings("unchecked")
+    public List<SfiPpFormRegion> getList() {
+        return (List<SfiPpFormRegion>) getCriteria()
+                .setProjection(Projections.alias(Projections.groupProperty("name"), "name"))
+                .addOrder(Order.asc("id"))
+                .setResultTransformer(Transformers.aliasToBean(SfiPpFormRegion.class))
+                .list();
+    }
 }
