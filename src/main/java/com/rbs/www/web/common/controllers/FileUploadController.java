305e765d58dd6f68b60c17526de7c41f308a0339
shahidulhasan <er.shahidul@gmail.com>
2016-02-16 07:12:08 +0600

    testing

--- src/main/java/com/rbs/www/web/common/controllers/FileUploadController.java
+++ src/main/java/com/rbs/www/web/common/controllers/FileUploadController.java
@@ -11,11 +11,13 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 import javax.servlet.http.HttpServletRequest;
+import java.util.Objects;
 
 @Controller
 public class FileUploadController {
 
     private final String[] validExtensions = { "pdf", "doc", "xls", "xlsx", "docx", "png","jpg" };
+    private final String[] validExtensionsSic = { "pdf", "doc", "xls", "xlsx", "docx" };
 
     @Autowired
     private BlobStoreHelper blobStoreHelper;
@@ -25,10 +27,17 @@ public class FileUploadController {
                                                            @RequestParam("file") MultipartFile file,
                                                            HttpServletRequest request) {
         BlobNames fileNames = null;
-        if (file != null && blobStoreHelper.isTypeOf(file.getOriginalFilename(), validExtensions))
-            fileNames = blobStoreHelper.store(file, form, request.getSession()
-                                                                 .getServletContext()
-                                                                 .getRealPath("."));
+        if (Objects.equals(form, "sic")){
+            if (file != null && blobStoreHelper.isTypeOf(file.getOriginalFilename(), validExtensionsSic))
+                fileNames = blobStoreHelper.store(file, form, request.getSession()
+                        .getServletContext()
+                        .getRealPath("."));
+        }else {
+            if (file != null && blobStoreHelper.isTypeOf(file.getOriginalFilename(), validExtensions))
+                fileNames = blobStoreHelper.store(file, form, request.getSession()
+                        .getServletContext()
+                        .getRealPath("."));
+        }
 
         HttpStatus responseStatus = fileNames == null ? HttpStatus.BAD_REQUEST : HttpStatus.OK;
         String responseMessage = fileNames == null ? "Invalid File Type" : Util.getAsString(fileNames);
