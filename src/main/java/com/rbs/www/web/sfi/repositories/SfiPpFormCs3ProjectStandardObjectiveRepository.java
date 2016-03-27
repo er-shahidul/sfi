5276d0041654ccadf56b71e99e7f11d432d2763c
shahidulhasan <er.shahidul@gmail.com>
2016-03-27 13:42:24 +0600

    ticket-742

--- src/main/java/com/rbs/www/web/sfi/repositories/SfiPpFormCs3ProjectStandardObjectiveRepository.java
+++ src/main/java/com/rbs/www/web/sfi/repositories/SfiPpFormCs3ProjectStandardObjectiveRepository.java
@@ -2,6 +2,7 @@ package com.rbs.www.web.sfi.repositories;
 
 import com.rbs.www.common.repositories.BaseRepository;
 import com.rbs.www.web.sfi.models.entities.SfiPpFormCs3ProjectStandardObjective;
+import org.hibernate.criterion.Restrictions;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -10,6 +11,10 @@ import java.util.List;
 public class SfiPpFormCs3ProjectStandardObjectiveRepository extends BaseRepository<SfiPpFormCs3ProjectStandardObjective> {
     @SuppressWarnings("unchecked")
     public List<SfiPpFormCs3ProjectStandardObjective> getAll() {
-        return getCriteria().list();
+        return getCriteria()
+                .add(Restrictions
+                        .and(Restrictions.isNotNull("sfiStandardObjDesc"),
+                             Restrictions.ne("sfiStandardObjDesc", "")))
+                .list();
     }
 }
