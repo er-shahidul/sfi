c765cf9dc86b5487d35b900a5b569cfecf2d8af1
masudrana2dsi <masud.it09@gmail.com>
2016-11-24 13:07:28 +0600

    777 1-13. Make dropdown multiselect return tickate resolve

--- src/main/webapp/assets/js/sicApp/controllers/SicCs1FormCtrl.js
+++ src/main/webapp/assets/js/sicApp/controllers/SicCs1FormCtrl.js
@@ -6,6 +6,12 @@ sfiSicApp
 
         $scope.cs1 = angular.copy($rootScope.form.cs1);
 
+        $scope.involvements = [  {id:1,name:'Attending'},
+                                {id:2,name:'Sponsoring'},
+                                {id:3,name:'Exhibiting'},
+                                {id:4,name:'Speaking'}
+                                ];
+
         $scope.guideFile = {}
         $scope.cs1.guideFiles = $scope.cs1.guideFiles || [];
 
@@ -31,13 +37,19 @@ sfiSicApp
         $scope.cs1.participatingMeetingList = $scope.cs1.participatingMeetingList || [];
         angular.forEach($scope.cs1.participatingMeetingList, function(value, index) {
             if(value.involvement){
-                value.involvement = value.involvement.split(',');
+                /*value.involvement = [];*/
+                value.involvement= JSON.parse(value.involvement);
+
+            }else{
+                value.involvement = [];
             }
+
         });
+
         $scope.addParticipatingMeeting = function(){
 
             if($scope.cs1Form.participatingMeetingForm.$valid){
-                $scope.cs1.participatingMeetingList.push({});
+                $scope.cs1.participatingMeetingList.push({involvement:[]});
             }
         }
         $scope.removeFromList = function(index,list){
@@ -59,18 +71,26 @@ sfiSicApp
 
         $scope.saveForm = function(go){
 
-            angular.forEach($scope.cs1.participatingMeetingList, function(value, index) {
-                value.involvement = value.involvement.toString();
-            });
             if($scope.cs1Form.$valid){
 
+                var data = angular.copy($scope.cs1);
+                angular.forEach(data.participatingMeetingList, function(value, index) {
+                    value.involvement= JSON.stringify(value.involvement);
+                });
+
                 $http
-                    .put("/sicForm/cs1", $scope.cs1)
+                    .put("/sicForm/cs1", data)
                     .then(function(response){
 
                         if(response.data){
+                            angular.forEach(response.data.participatingMeetingList, function(value, index) {
+                                if(value.involvement){
+                                    value.involvement= JSON.parse(value.involvement);
+                                }
+                            });
                             $scope.cs1Form.$setPristine();
                             $rootScope.form.cs1 = response.data;
+
                             $scope.errors = $rootScope.form.cs1.errors;
                             Message.success('Section successfully saved', '.msg-cont', true);
 
@@ -79,7 +99,12 @@ sfiSicApp
                         }
 
                         if(go) $rootScope.goStep("cs2");
+                    }, function (err) {
+                        console.error(err);
+
                     });
+            }else{
+                console.log('invalid');
             }
         }
 
