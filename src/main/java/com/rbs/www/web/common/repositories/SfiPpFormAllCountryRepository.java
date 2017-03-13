59aef5585b50443f9df5c9727cf233a58ea1acd8
shahidulhasan <er.shahidul@gmail.com>
2017-03-13 12:56:12 +0600

    ticket-1022

--- src/main/java/com/rbs/www/web/common/repositories/SfiPpFormAllCountryRepository.java
+++ src/main/java/com/rbs/www/web/common/repositories/SfiPpFormAllCountryRepository.java
@@ -2,8 +2,22 @@ package com.rbs.www.web.common.repositories;
 
 import com.rbs.www.common.repositories.BaseRepository;
 import com.rbs.www.web.common.models.entities.SfiPpFormAllCountry;
+import com.rbs.www.web.sfi.models.entities.SfiPpForm2014;
+import org.hibernate.criterion.Order;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 @Repository
 public class SfiPpFormAllCountryRepository extends BaseRepository<SfiPpFormAllCountry> {
+
+    @Transactional
+    @SuppressWarnings("unchecked")
+    public List<SfiPpFormAllCountry> getAll(){
+        return (List<SfiPpFormAllCountry>) getCriteria()
+                .addOrder(Order.asc("country"))
+                .list();
+    }
+
 }
