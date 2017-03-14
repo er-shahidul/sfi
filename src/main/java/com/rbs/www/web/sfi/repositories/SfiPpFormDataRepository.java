9e5898e572901fce78fa6d9a65efa91a96f13597
shahidulhasan <er.shahidul@gmail.com>
2017-03-14 11:29:06 +0600

    ticket-993

--- src/main/java/com/rbs/www/web/sfi/repositories/SfiPpFormDataRepository.java
+++ src/main/java/com/rbs/www/web/sfi/repositories/SfiPpFormDataRepository.java
@@ -3,11 +3,14 @@ package com.rbs.www.web.sfi.repositories;
 import com.rbs.www.admin.models.entities.Company;
 import com.rbs.www.common.repositories.BaseRepository;
 import com.rbs.www.web.sfi.models.entities.SfiPpFormData;
+import org.hibernate.criterion.Order;
 import org.hibernate.criterion.Projections;
 import org.hibernate.criterion.Restrictions;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Date;
+import java.util.List;
 
 @Repository
 public class SfiPpFormDataRepository extends BaseRepository<SfiPpFormData> {
@@ -22,5 +25,15 @@ public class SfiPpFormDataRepository extends BaseRepository<SfiPpFormData> {
                 .add(Restrictions.eq("id", id))
                 .setProjection(Projections.property("updatedAt"))
                 .uniqueResult();
+    }
+
+    @Transactional
+    @SuppressWarnings("unchecked")
+    public List<SfiPpFormData> getAllActiveForms(){
+        return (List<SfiPpFormData>) getCriteria()
+                .createAlias("company", "company")
+                .add(Restrictions.eq("company.sfi", true))
+                .addOrder(Order.desc("id"))
+                .list();
     }
 }
--- src/main/java/com/rbs/www/web/sic/repositories/SicFormDataRepository.java
+++ src/main/java/com/rbs/www/web/sic/repositories/SicFormDataRepository.java
@@ -3,11 +3,14 @@ package com.rbs.www.web.sic.repositories;
 import com.rbs.www.admin.models.entities.Company;
 import com.rbs.www.common.repositories.BaseRepository;
 import com.rbs.www.web.sic.models.entities.SicFormData;
+import org.hibernate.criterion.Order;
 import org.hibernate.criterion.Projections;
 import org.hibernate.criterion.Restrictions;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Date;
+import java.util.List;
 
 @Repository
 public class SicFormDataRepository extends BaseRepository<SicFormData> {
@@ -22,5 +25,15 @@ public class SicFormDataRepository extends BaseRepository<SicFormData> {
                 .add(Restrictions.eq("id", id))
                 .setProjection(Projections.property("updatedAt"))
                 .uniqueResult();
+    }
+
+    @Transactional
+    @SuppressWarnings("unchecked")
+    public List<SicFormData> getAllActiveForms(){
+        return (List<SicFormData>) getCriteria()
+                .createAlias("company", "company")
+                .add(Restrictions.eq("company.sci", true))
+                .addOrder(Order.desc("id"))
+                .list();
     }
 }
